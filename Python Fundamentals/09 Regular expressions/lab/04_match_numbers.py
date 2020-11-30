cd0b5bf91c21c261426f58540ff89fe96da18588
Dilyan Tsenkov <71652510+DilyanTsenkov@users.noreply.github.com>
2020-11-30 17:18:09 +0200

    Delete 04_match_numbers.py

--- Python Fundamentals/09 Regular expressions/lab/04_match_numbers.py
+++
@@ -1,9 +0,0 @@
-import re
-
-numbers = input()
-regex = r"(^|(?<= ))-?[0-9]+(\.[0-9]+)?($|(?= ))"
-
-numbers = re.finditer(regex, numbers)
-numbers = [n.group() for n in numbers]
-
-print(" ".join(numbers))
