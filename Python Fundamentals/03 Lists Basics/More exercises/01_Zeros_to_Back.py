0a0d83faa41001e3c8d08b7ab249070b273a93f4
Dilyan Tsenkov <71652510+DilyanTsenkov@users.noreply.github.com>
2020-11-30 16:33:24 +0200

    Create 01_Zeros_to_Back.py

---
+++ Python Fundamentals/03 Lists Basics/More exercises/01_Zeros_to_Back.py
@@ -0,0 +1,12 @@
+single_string = input()
+
+single_string_list = single_string.split(", ")
+single_string_list.append(single_string_list.pop(single_string_list.index("0")))
+
+for i in range(len(single_string_list)):
+    single_string_list[i] = int(single_string_list[i])
+for i in range(len(single_string_list)):
+    if single_string_list[i] == 0:
+        single_string_list.append(single_string_list.pop(single_string_list.index(0)))
+        
+print(single_string_list)
