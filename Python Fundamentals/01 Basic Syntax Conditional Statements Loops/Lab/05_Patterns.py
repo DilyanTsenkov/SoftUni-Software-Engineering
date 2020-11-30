877e2bbf48f4c5aef023f3502a0a592ba9fc176e
Dilyan Tsenkov <71652510+DilyanTsenkov@users.noreply.github.com>
2020-11-30 16:07:03 +0200

    Add files via upload

---
+++ Python Fundamentals/01 Basic Syntax Conditional Statements Loops/Lab/05_Patterns.py
@@ -0,0 +1,9 @@
+n = int(input())
+for row in range(1, n + 1):
+    for col in range(row):
+        print("*", end="")
+    print()
+for row in range(n - 1, 0, -1):
+    for col in range(row):
+        print("*", end="")
+    print()
