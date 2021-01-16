107928c1014b88b168e6431a1261711bae9c94d3
Dilyan Tsenkov <71652510+DilyanTsenkov@users.noreply.github.com>
2021-01-16 17:22:26 +0200

    Add files via upload

--- Python_Advanced/02_Tuples_and_Sets/Exercise/07_battle_of_names.py
+++ Python_Advanced/02_Tuples_and_Sets/Exercise/07_battle_of_names.py
@@ -1,31 +1,43 @@
-n = int(input())
+def odd_even_checker(n_fun):
+    even_fun = set()
+    odd_fun = set()
+    for number in range(1, n_fun + 1):
+        name = input()
+        ascii_sum = 0
+        for ch in name:
+            ascii_sum += ord(ch)
+        result = ascii_sum // number
+        if result % 2 == 0:
+            even_fun.add(result)
+        else:
+            odd_fun.add(result)
+    return odd_fun, even_fun
 
-even = set()
-odd = set()
 
-for number in range(1, n + 1):
-    name = input()
-    ascii_sum = 0
+def sum_odd_even(odd_fun, even_fun):
+    odd_sum_fun = sum(odd_fun)
+    even_sum_fun = sum(even_fun)
+    return odd_sum_fun, even_sum_fun
 
-    for ch in name:
-        ascii_sum += ord(ch)
-    result = ascii_sum // number
 
-    if result % 2 == 0:
-        even.add(result)
+def action(odd_fun, even_fun):
+    union_fun = odd_fun.union(even_fun)
+    different_fun = odd_fun.difference(even_fun)
+    symmetric_fun = odd_fun.symmetric_difference(even_fun)
+    return union_fun, different_fun, symmetric_fun
+
+
+def printer(odd_sum_fun, even_sum_fun, union_fun, different_fun, symmetric_fun):
+    if odd_sum_fun == even_sum_fun:
+        print(*union_fun, sep=", ")
+    elif odd_sum_fun > even_sum_fun:
+        print(*different_fun, sep=", ")
     else:
-        odd.add(result)
+        print(*symmetric_fun, sep=", ")
 
-odd_sum = sum(odd)
-even_sum = sum(even)
 
-union = odd.union(even)
-different = odd.difference(even)
-symmetric = odd.symmetric_difference(even)
-
-if odd_sum == even_sum:
-    print(*union, sep=", ")
-elif odd_sum > even_sum:
-    print(*different, sep=", ")
-else:
-    print(*symmetric, sep=", ")
+n = int(input())
+odd, even = odd_even_checker(n)
+odd_sum, even_sum = sum_odd_even(odd, even)
+union, different, symmetric = action(odd, even)
+printer(odd_sum, even_sum, union, different, symmetric)
