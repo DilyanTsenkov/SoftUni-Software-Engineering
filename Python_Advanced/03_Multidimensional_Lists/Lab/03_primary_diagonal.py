74c76c35981912143d85eb8b19a7ea4a62182b9b
Dilyan Tsenkov <71652510+DilyanTsenkov@users.noreply.github.com>
2021-01-18 19:41:16 +0200

    Add files via upload

--- Python_Advanced/03_Multidimensional_Lists/Lab/03_primary_diagonal.py
+++ Python_Advanced/03_Multidimensional_Lists/Lab/03_primary_diagonal.py
@@ -1,13 +1,18 @@
+def matrix_creator(rows_fun):
+    matrix_fun = []
+    for _ in range(rows_fun):
+        line = [int(x) for x in input().split()]
+        matrix_fun.append(line)
+    return matrix_fun
+
+
+def diagonal_sum(matrix_fun, rows_fun):
+    result = 0
+    for row in range(rows_fun):
+        result += matrix_fun[row][row]
+    print(result)
+
+
 rows = int(input())
-
-matrix = []
-
-for _ in range(rows):
-    line = [int(x) for x in input().split()]
-    matrix.append(line)
-
-diagonal_sum = 0
-for row in range(rows):
-    diagonal_sum += matrix[row][row]
-
-print(diagonal_sum)
+matrix = matrix_creator(rows)
+diagonal_sum(matrix, rows)
