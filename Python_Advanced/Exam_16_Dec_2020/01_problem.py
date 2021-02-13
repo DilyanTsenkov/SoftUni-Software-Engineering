aa9863a23ccebb81dbfdfd3c26d01872f1776ac5
Dilyan Tsenkov <71652510+DilyanTsenkov@users.noreply.github.com>
2021-02-13 18:31:26 +0200

    Add files via upload

--- Python_Advanced/Exam_16_Dec_2020/01_problem.py
+++ Python_Advanced/Exam_16_Dec_2020/01_problem.py
@@ -1,32 +1,27 @@
 from collections import deque
 
-males = deque(int(el) for el in input().split())
-females = deque(int(el) for el in input().split())
 
-matches = 0
-while males and females:
-    while males and males[-1] <= 0:
-        males.pop()
-    if not males:
-        break
-    while females and females[0] <= 0:
-        females.popleft()
-    if not females:
-        break
-    while males and males[-1] % 25 == 0:
-        males.pop()
-        if not males:
+def female_male_0(females_males):
+    while females_males and females_males[-1] <= 0:
+        females_males.pop()
+    return females_males
+
+
+def female_male_25(females_males):
+    while females_males and females_males[-1] % 25 == 0:
+        females_males.pop()
+        if not females_males:
             break
-        males.pop()
-    if not males:
-        break
-    while females and females[-1] % 25 == 0:
-        females.popleft()
-        if not females:
-            break
-        females.popleft()
-    if not females:
-        break
+        females_males.pop()
+    return females_males
+
+
+def no_males_or_females(females_males):
+    if not females_males:
+        return True
+
+
+def male_female_compare(males, females, matches):
     if males[-1] == females[0]:
         males.pop()
         females.popleft()
@@ -36,16 +31,38 @@ while males and females:
         male -= 2
         males.append(male)
         females.popleft()
+    return males, females, matches
 
-males = [str(male) for male in males][::-1]
-females = [str(female) for female in females]
 
-print(f"Matches: {matches}")
-if males:
-    print(f"Males left: {', '.join(males)}")
-else:
-    print("Males left: none")
-if females:
-    print(f"Females left: {', '.join(females)}")
-else:
-    print("Females left: none")
+def printer(males, females, matches):
+    males = [str(male) for male in males][::-1]
+    females = [str(female) for female in females]
+    print(f"Matches: {matches}")
+    if males:
+        print(f"Males left: {', '.join(males)}")
+    else:
+        print("Males left: none")
+    if females:
+        print(f"Females left: {', '.join(females)}")
+    else:
+        print("Females left: none")
+
+
+males = deque(int(el) for el in input().split())
+females = deque(int(el) for el in input().split())
+matches = 0
+while males and females:
+    males = female_male_0(males)
+    if no_males_or_females(males):
+        break
+    females = female_male_0(females)
+    if no_males_or_females(females):
+        break
+    female_male_25(males)
+    if no_males_or_females(males):
+        break
+    female_male_25(females)
+    if no_males_or_females(females):
+        break
+    males, females, matches = male_female_compare(males, females, matches)
+printer(males, females, matches)
